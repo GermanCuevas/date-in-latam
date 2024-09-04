47796bb9dd16404ca84809897ed6b734d2909498
German <gcuevas@insideone.cloud>
2024-09-04 16:28:17 -0300

    add i18n select

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,15 +1,25 @@
+//"use client"
 import Image from "next/image";
-import BoxRoot from "../components/BoxRoot";
+import BoxRoot from "../../components/BoxRoot";
 import "./globals.css";
-//className="flex items-center justify-center relative h-screen "
-export default function Home() {
+
+//import { useTranslation } from "../i18n"; //server
+
+//import {useTranslation} from "../i18n/client.js" //client
+
+//className="flex items-center justify-center relative h-screen"
+
+export default async function Home({ params: { lng } }) {
+  // console.log("lng ==>", lng);
+  // console.log("useTranslation =>", useTranslation);
+  // const { t:traductor } = await useTranslation(lng, "about")
   const bg = "bg-gradientLight dark:bg-gradientDark";
 
   return (
     <div className={`flex items-center justify-center relative h-screen ${bg} `}>
       <div className="flex flex-col gap-24 sm:gap-16 items-center">
         <Image src="/assets/LogoDate.svg" width={275} height={161} alt="Data in latam Logo" className="sm:w-[339px] sm:h-[203px]" />
-        <BoxRoot />
+        <BoxRoot lng={lng} />
       </div>
     </div>
   );
--- src/components/BoxRoot.tsx
+++ src/components/BoxRoot.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation.js';
 import Button from '../commons/Button'
+import { useTranslation } from '@/app/i18n/client.js';
 
 //Forma mas estricta de validar ruta con next y typescript:
 //import { RoutePath } from 'react-router-dom';
@@ -8,7 +9,13 @@ import Button from '../commons/Button'
 //instalar: npm install --save-dev @types/react-router-dom
 //border-myColorBlack-500 dark:border-vibrant-50
 
-const BoxRoot = () => {
+const BoxRoot = ({lng}) => {
+  //console.log("lng ==>",lng);
+  //console.log(useTranslation);
+  
+  const { t:traductor } = useTranslation(lng, "boxRoot")
+  //console.log(traductor);
+  
   const router = useRouter()
   const redirectionHandle = (to: string)=>{
     router.push(to)
@@ -17,8 +24,8 @@ const BoxRoot = () => {
   //text-base
   return (
     <div className="flex flex-col p-5 shadow-xl rounded-md gap-y-8 bg-myColorTransparent-500">
-      <Button type="button" text={"Regístrese para comenzar el viaje 😄"} variant={"primary"} to="register" handleFunction={redirectionHandle} fontSize={"normal"} />
-      <Button type="button" text={"¡ Ya tengo cuenta !"} variant={"secondary"} to="login" handleFunction={redirectionHandle} fontSize={"normal"}/>
+      <Button type="button" text={traductor("register")} variant={"primary"} to="register" handleFunction={redirectionHandle} fontSize={"normal"} />
+      <Button type="button" text={"¡Ya tengo cuenta!"} variant={"secondary"} to="login" handleFunction={redirectionHandle} fontSize={"normal"}/>
       <Button type="button" text={"Solo quiero hechar un vistazo  👀"} variant={"secondary"} to="welcome" handleFunction={redirectionHandle} fontSize={"normal"}/>
     </div>
   );
