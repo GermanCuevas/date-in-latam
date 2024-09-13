b9a276976760a5e6b834aaf73b389a38d78902fd
German <gcuevas@insideone.cloud>
2024-09-13 18:17:40 -0300

    hot fix

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -20,9 +20,9 @@ export default async function Home({ params: { lng } }: HomeProps) {
   // console.log("useTranslation =>", useTranslation);
   // const { t:traductor } = await useTranslation(lng, "about")
   const bg = "bg-gradientLight dark:bg-gradientDark";
-
+//`flex items-center justify-center relative min-h-screen ${bg}`
   return (
-    <div className={`flex items-center justify-center relative h-screen ${bg} `}>
+    <div className={`flex items-center justify-center relative min-h-screen ${bg}`}>
       <div className="flex flex-col gap-24 sm:gap-16 items-center">
         <Image src="/assets/LogoDate.svg" width={275} height={161} alt="Data in latam Logo" className="sm:w-[339px] sm:h-[203px]" />
         <BoxRoot lng={lng} />
